Objects can now impart velocity to the fluid thanks to a technique called Optical Flow (making use of another OpenCV module). Fixed up an issue with the gradient subtraction being applied to solids.

SimBase.py
@@ -45,23 +45,32 @@ class SimBase:
         self._mode = value
 
     @jit
-    def set_boundary(self, cell_ocupation, flow_direction):
+    def set_boundary(self, cell_ocupation, boundary_velocity, flow_direction):
         # input boundary
         self._b[:] = cell_ocupation
+        self._v[:, self._b] = boundary_velocity[:, self._b]
 
         # add boundary walls parrallel to flow
         if flow_direction == 0:
             self._b[:, :1] = True
             self._b[:, -1:] = True
+            self._v[:, :, :1] = 0
+            self._v[:, :, -1:] = 0
         elif flow_direction == 1:
             self._b[:1, :] = True
             self._b[-1:, :] = True
+            self._v[:, :1, :] = 0
+            self._v[:, -1:, :] = 0
         elif flow_direction == 2:
             self._b[:, :1] = True
             self._b[:, -1:] = True
+            self._v[:, :, :1] = 0
+            self._v[:, :, -1:] = 0
         elif flow_direction == 3:
             self._b[:1, :] = True
             self._b[-1:, :] = True
+            self._v[:, :1, :] = 0
+            self._v[:, -1:, :] = 0
 
         # cache boundary inverse
         self._notb = np.logical_not(self._b)
@@ -76,13 +85,21 @@ class SimBase:
         if flow_direction == 0:
             self._v[0, :flowwidth, :] = flow_speed
             self._v[0, -flowwidth:, :] = flow_speed
+            self._v[1, :flowwidth, :] = 0
+            self._v[1, -flowwidth:, :] = 0
         elif flow_direction == 1:
+            self._v[0, :, :flowwidth] = 0
+            self._v[0, :, -flowwidth:] = 0
             self._v[1, :, :flowwidth] = flow_speed
             self._v[1, :, -flowwidth:] = flow_speed
         elif flow_direction == 2:
             self._v[0, :flowwidth, :] = -flow_speed
             self._v[0, -flowwidth:, :] = -flow_speed
+            self._v[1, :flowwidth, :] = 0
+            self._v[1, -flowwidth:, :] = 0
         elif flow_direction == 3:
+            self._v[0, :, :flowwidth] = 0
+            self._v[0, :, -flowwidth:] = 0
             self._v[1, :, :flowwidth] = -flow_speed
             self._v[1, :, -flowwidth:] = -flow_speed
 
@@ -103,6 +120,7 @@ class SimBase:
     def get_velocity_field_as_HSV(self, power=0.5):
         # map velocity per grid point to hue (direction) and value (magnitude)
         self._hsv_field[0] = 180 * (np.arctan2(-self._v[0], -self._v[1]) / (2 * np.pi) + 0.5)
+        # self._hsv_field[1] = 255
         self._hsv_field[2] = 255 * (self._v[0]**2 + self._v[1]**2) ** power
         return self._hsv_field
 
@@ -115,7 +133,7 @@ class SimBase:
             self.step(dt, [])
 
     @jit
-    def render(self, camera, render_mask=False):
+    def render(self, camera, render_mask_velocity=False, render_mask=False):
         # update and render the sim
         if self._mode == 0:
             # combine the density fields with the input camera frame
@@ -128,18 +146,21 @@ class SimBase:
                 self._output[:camera.shape[1]//4,:camera.shape[0]//4] = \
                     cv2.resize(camera.mask, (camera.shape[0]//4, camera.shape[1]//4))[:,:,np.newaxis]
         else:
-            # render the pressure solution as range from blue (negative) to red (positive)
-            self._simshapetmp[:] = self.get_pressure_as_rgb().T * 512
-            cv2.resize(self._simshapetmp, camera.shape, dst=self._camshapetmp)
-            cv2.blur(self._camshapetmp, (20, 20), dst=self._camshapetmp)
-            # add the the cam mask
-            cv2.add(cv2.cvtColor(camera.mask // 8, cv2.COLOR_GRAY2BGR), self._camshapetmp, dst=self._camshapetmp)
 
             # render the velocity field
             cv2.cvtColor(self.get_velocity_field_as_HSV(0.25).T, cv2.COLOR_HSV2BGR, dst=self._simshapetmp) # should be 0.5 (i.e. square root), but this shows the lower velocities better
             cv2.resize(self._simshapetmp, camera.shape, dst=self._output)
 
-            # combine using the mask to select between the two renders
-            cv2.add(self._output, self._camshapetmp, dst=self._output, mask=camera.mask)
+            if render_mask:
+                # render the pressure solution as range from blue (negative) to red (positive)
+                # self._simshapetmp[:] = self.get_pressure_as_rgb().T * 512
+                # cv2.resize(self._simshapetmp, camera.shape, dst=self._camshapetmp)
+                # cv2.blur(self._camshapetmp, (20, 20), dst=self._camshapetmp)
+                self._camshapetmp[:] = 0
+                # add the the cam mask
+                cv2.add(cv2.cvtColor(camera.mask // 4, cv2.COLOR_GRAY2BGR), self._camshapetmp, dst=self._camshapetmp)
+
+                # combine using the mask to select between the two renders
+                cv2.add(self._output, self._camshapetmp, dst=self._output, mask=camera.mask)
 
         return self._output

run.py
@@ -32,6 +32,7 @@ render_mask = Options.Cycle('Render Mask', 'm', ['false', 'true'], configuration
 bg_mode = Options.Cycle('BG', 'b', ['white', 'black', 'hue', 'bg subtract'], configuration.bg_mode)
 mask_level = Options.Range('Mask Threshold', ['1','2'], [0, 1], 0.03, configuration.mask_level)
 mask_width = Options.Range('Mask Width', ['3','4'], [0, 0.5], 0.01, configuration.mask_width)
+optical_flow = Options.Cycle('Optical Flow', 'o', ['false', 'true'], configuration.optical_flow)
 
 sim_res_multiplier = Options.Range('Sim Res', ['9','0'], [0.1, 2.0], 0.1, configuration.sim_res_multiplier)
 flow_speed = Options.Range('Flow Speed', ['-','='], [0.02, 1], 0.02, configuration.flow_speed)
@@ -43,7 +44,7 @@ debugMode = Options.Cycle('Mode', 'd', ['Normal', 'Debug'], 0)
 
 # add to a list to update and display
 options = [fullscreen, mirror_screen, render_mask,
-    bg_mode, mask_level, mask_width,
+    bg_mode, mask_level, mask_width, optical_flow,
     sim_res_multiplier, flow_speed, flow_direction, num_smoke_streams, smoke_percentage,
     debugMode]
 
@@ -67,6 +68,8 @@ def run_sim():
         # fluid sim resolution. Update when option changes.
         if sim_res_multiplier.get_has_changed(reset_change_flag=True):
             sim = configuration.Sim(camera.shape, sim_res_multiplier.current, 0, 0)
+            sim.mode = debugMode.current
+            flow = np.zeros((sim.shape[1], sim.shape[0], 2))
 
         # Always True on first iteration. Update fullscreen if option changed
         if fullscreen.get_has_changed(reset_change_flag=True):
@@ -87,17 +90,30 @@ def run_sim():
         camera.update(bg_mode.current, mirror_screen.current,
             mask_level.current, mask_width.current)
 
+
+        if optical_flow.current == 1:
+            flow[:] = cv2.calcOpticalFlowFarneback(cv2.resize(camera.last_grey, sim.shape),
+                                                cv2.resize(camera.current_grey, sim.shape),
+                                                float(0),
+                                                pyr_scale=0.5, levels=3, winsize=15, iterations=3,
+                                                poly_n=5, poly_sigma=1.0, flags=0)
+
+            scale = np.sqrt(sim._dx[0] * sim._dx[1]) / fps.last_dt
+            flow *= scale
+        else:
+            flow[:] = 0
+
         sim.set_velocity(fps.last_dt, flow_speed.current,
             flow_direction.current, num_smoke_streams.current,
             smoke_percentage.current)
 
         # copy the webcam generated mask into the boundary
         boundary = np.array(cv2.resize(camera.mask, sim.shape).T, dtype=bool)
-        sim.set_boundary(boundary, flow_direction.current)
+        sim.set_boundary(boundary, flow.T, flow_direction.current)
 
         # update and render the sim
         sim.udpate(fps.last_dt)
-        output = sim.render(camera, render_mask=(render_mask.current == 1))
+        output = sim.render(camera, render_mask_velocity=(optical_flow.current==1), render_mask=(render_mask.current == 1))
         output_shape = np.shape(output)
 
         # add the GUI

util/Camera.py
@@ -6,7 +6,7 @@ random = np.array(np.power(np.random.rand(16, 8, 3), 3) * 255, dtype=np.uint8)
 
 class Camera:
 
-    def _resize_frame(self, frame, flip=0):
+    def _resize_frame(self, frame, dst, flip=0):
         frame_shape = np.shape(frame)
         frame_crop_height = int(frame_shape[1] / self._ratio)
         crop_offset = (frame_shape[0] - frame_crop_height) // 2
@@ -16,13 +16,13 @@ class Camera:
             cropped_frame = frame
 
         if flip == 1: # horizontal
-            return cv2.resize(cv2.flip(cropped_frame, 1), self._size)
+            cv2.resize(cv2.flip(cropped_frame, 1), self._size, dst=dst)
         elif flip == 2: # verticle
-            return cv2.resize(cv2.flip(cropped_frame, 0), self._size)
+            cv2.resize(cv2.flip(cropped_frame, 0), self._size, dst=dst)
         elif flip == 3: # both
-            return cv2.resize(cv2.flip(cropped_frame, -1), self._size)
+            cv2.resize(cv2.flip(cropped_frame, -1), self._size, dst=dst)
         else:
-            return cv2.resize(cropped_frame, self._size)
+            cv2.resize(cropped_frame, self._size, dst=dst)
 
     def __init__(self, size=(640,360), camera_index=0, no_cam_allowed=False):
         self._no_cam_allowed = no_cam_allowed
@@ -34,10 +34,13 @@ class Camera:
         self._input_frame = np.zeros((*self._size[::-1], 3), dtype=np.uint8)
         self._hsv_field = np.zeros((*self._size[::-1], 3), dtype=np.uint8)
 
+        self._last_grey = np.zeros(self._size[::-1], dtype=np.uint8)
+        self._current_grey = np.zeros(self._size[::-1], dtype=np.uint8)
+
         if not self._cap.isOpened():
 
             # random = np.array(np.power(np.random.rand(16, 8, 3), 3) * 255, dtype=np.uint8)
-            self._input_frame = self._resize_frame(random)
+            self._resize_frame(random, dst=self._input_frame)
 
             ''' HSV test image
             test_image = np.zeros_like(self._input_frame, dtype=np.uint8)
@@ -58,16 +61,19 @@ class Camera:
             # update frame if webcam is active
             ret, frame = self._cap.read()
             if ret:
-                self._input_frame = self._resize_frame(frame, mirror_screen)
+                self._resize_frame(frame, self._input_frame, mirror_screen)
         else:
             # else use a random image
-            self._input_frame = self._resize_frame(random, mirror_screen)
+            self._resize_frame(random, self._input_frame, mirror_screen)
+
+        self._last_grey[:] = self._current_grey
+        cv2.cvtColor(self._input_frame, cv2.COLOR_BGR2GRAY, dst=self._current_grey)
 
         if bg_option == 3: # background subtraction
             self._mask[:] = self._fgbg.apply(self._input_frame, learningRate=0.003)
         else:
             self._mask[:] = 0
-            self._hsv_field[:] = cv2.cvtColor(self._input_frame, cv2.COLOR_BGR2HSV)
+            cv2.cvtColor(self._input_frame, cv2.COLOR_BGR2HSV, dst=self._hsv_field)
             if bg_option == 2: # hue
                 x = np.abs(np.array(self._hsv_field[:,:,0], np.float) / 180 - mask_level)
                 self._mask[x > mask_width] = 255
@@ -92,6 +98,8 @@ class Camera:
     def shape(self):
         return self._size
 
+
+
     @property
     def input_frame(self):
         return self._input_frame
@@ -99,6 +107,14 @@ class Camera:
     @property
     def mask(self):
         return self._mask
+
+    @property
+    def current_grey(self):
+        return self._current_grey
+
+    @property
+    def last_grey(self):
+        return self._last_grey
 
     @jit
     def get_mask(self, size, transpose):